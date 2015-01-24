1f6a55662d924b16a62274a49c8118fe77e9f8a7
Sid Haniff <sid@truenorth.gb.com>
2015-01-24 08:56:32 +0000

    New demo class

---
+++ src/main/java/eu/waldonia/cspark/SimpleCBAnalyser.java
@@ -0,0 +1,16 @@
+package eu.waldonia.cspark;
+
+import org.apache.spark.SparkConf;
+
+public class SimpleCBAnalyser {
+    
+    // used to access the Spark Context
+    private SparkConf conf;
+    
+    
+    public void init() {
+	
+    }
+    
+
+}
