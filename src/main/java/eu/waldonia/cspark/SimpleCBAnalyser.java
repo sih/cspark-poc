114fd874cd19b17684911b295825907a148ec807
Sid Haniff <sid@truenorth.gb.com>
2015-01-24 10:23:26 +0000

    Simple working example

--- src/main/java/eu/waldonia/cspark/SimpleCBAnalyser.java
+++ src/main/java/eu/waldonia/cspark/SimpleCBAnalyser.java
@@ -1,16 +1,45 @@
 package eu.waldonia.cspark;
 
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapColumnTo;
+
+import java.util.List;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.*;
+
+import com.datastax.spark.connector.japi.CassandraJavaUtil;
 
 public class SimpleCBAnalyser {
-    
+
     // used to access the Spark Context
-    private SparkConf conf;
-    
-    
-    public void init() {
-	
+    private JavaSparkContext sc;
+
+    public JavaSparkContext getContext() {
+	if (null == sc) {
+	    SparkConf conf = new SparkConf()
+	    .setMaster("local")
+	    .set("spark.cassandra.connection.host", "localhost")
+            .setAppName("Counting Journeys")
+            .set("spark.executor.memory", "1g");
+	    sc = new JavaSparkContext(conf);
+	}
+	return sc;
     }
-    
+
+    public static void main(String[] args) {
+	SimpleCBAnalyser a = new SimpleCBAnalyser();
+	JavaSparkContext sc = a.getContext();
+	
+	JavaRDD<String> journeys = CassandraJavaUtil.javaFunctions(sc)
+		.cassandraTable("traffic", "journeys", mapColumnTo(String.class))
+		.select("k_location_id");
+	
+	int count = 0;
+	List<String> locationIds = journeys.collect();
+	for (String lid : locationIds) {
+	    System.out.println(lid);
+	    if (++count > 10) break;
+	}
+    }
 
 }
